d98c5a5bbedeca9bf0656498c3515d0728afa85e
Deepak Bhardwaj <deepakb339@gmail.com>
2020-11-15 08:38:55 +0530

    first commit

---
+++ database/ecommerce.sql
@@ -0,0 +1,157 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:8889
+-- Generation Time: Jun 17, 2019 at 12:38 PM
+-- Server version: 5.7.23
+-- PHP Version: 7.2.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+--
+-- Database: `ecommerce`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `items`
+--
+
+CREATE TABLE `items` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `price` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `items`
+--
+
+INSERT INTO `items` (`id`, `name`, `price`) VALUES
+(1, 'IPhone 6s', 40000),
+(2, 'IPhone 7', 60000),
+(3, 'IPhone 7 Plus', 69000),
+(4, 'IPhone X', 88000),
+(5, 'Samsung Galaxy S7 EDGE', 45000),
+(6, 'Samsung Galaxy S8 PLUS', 65000),
+(7, 'Samsung Galaxy NOTE 8 PLUS', 60000),
+(8, 'Samsung Galaxy A8 ', 35000),
+(9, 'ONEPLUS 3', 28000),
+(10, 'ONEPLUS 3T', 31000),
+(11, 'ONEPLUS 5', 34000),
+(12, 'ONEPLUS 5T', 36000),
+(13, 'XIAOMI REDMI NOTE 4', 13000),
+(14, 'MI A1', 15000),
+(15, 'MI MAX 2', 16000),
+(16, 'MI MIX 2', 36000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `contact` varchar(255) NOT NULL,
+  `city` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `contact`, `city`, `address`) VALUES
+(1, 'Deepak Bhardwaj', 'deepakb339@gmail.com', '498b5924adc469aa7b660f457e0fc7e5', '9090909090', 'Shillong', 'in front of assam house, Dhankheti, Shillong, Meghalaya'),
+(2, 'amit deka', 'amit@deka.com', '498b5924adc469aa7b660f457e0fc7e5', '9898989898', 'nalbari', 'assam'),
+(3, 'Barry Allen', 'barryallen@gmail.com', '498b5924adc469aa7b660f457e0fc7e5', '9878987898', 'USA', 'Central City'),
+(4, 'deep b', 'dee334@gmail.com', '498b5924adc469aa7b660f457e0fc7e5', '9999999999', 'far', '212');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users_items`
+--
+
+CREATE TABLE `users_items` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `item_id` int(11) NOT NULL,
+  `status` enum('Added to cart','Confirmed') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users_items`
+--
+
+INSERT INTO `users_items` (`id`, `user_id`, `item_id`, `status`) VALUES
+(3, 1, 3, 'Confirmed'),
+(4, 2, 4, 'Added to cart'),
+(5, 1, 4, 'Confirmed'),
+(8, 1, 4, 'Added to cart'),
+(10, 1, 2, 'Added to cart'),
+(11, 1, 1, 'Added to cart');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `items`
+--
+ALTER TABLE `items`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users_items`
+--
+ALTER TABLE `users_items`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `item_id` (`item_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `items`
+--
+ALTER TABLE `items`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `users_items`
+--
+ALTER TABLE `users_items`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `users_items`
+--
+ALTER TABLE `users_items`
+  ADD CONSTRAINT `users_items_ibfk_1` FOREIGN KEY (`item_id`) REFERENCES `items` (`id`),
+  ADD CONSTRAINT `users_items_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
